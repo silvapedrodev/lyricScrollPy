0213d0cf86e9336615e4bf18b5a694d430d58ab0
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2025-09-05 15:08:06 -0300

    feat: implement show_header

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ bg_text = Fore.LIGHTBLACK_EX
 highlight = Style.BRIGHT + Fore.CYAN # <- choose color here
 
 init(autoreset=True)
+
+# Functions
+def show_header(title, desc):
+    print("\n")
+    print(title)
+    if desc:
+        print(desc)
+    print("-")
