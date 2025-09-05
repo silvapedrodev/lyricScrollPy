09ae37c3a60ac068b6ac6d8c19a62a82098f7f4f
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2025-09-05 16:10:57 -0300

    feat: create rander lyrics functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from time import sleep
 from colorama import init, Style, Fore, Cursor
 
 # Color
@@ -7,9 +9,31 @@ highlight = Style.BRIGHT + Fore.CYAN # <- choose color here
 init(autoreset=True)
 
 # Functions
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
+
 def show_header(title, desc):
     print("\n")
     print(title)
     if desc:
         print(desc)
     print("-")
+
+def render_lyrics(lyrics, current_index, window):
+    start = max(0, current_index - 2)
+    end = min(len(lyrics), current_index + window)
+
+    for i in range(start, end):
+        verse, _ = lyrics[i]
+        if i == current_index:
+            print(highlight + verse)
+        else:
+            print(bg_text + verse)
+
+def show_lyrics(title, desc, lyrics):
+    window_size = 4
+    for i, (_, delay) in enumerate(lyrics):
+        clear_console()
+        show_header(title, desc)
+        render_lyrics(lyrics, i, window_size)
+        sleep(delay)
